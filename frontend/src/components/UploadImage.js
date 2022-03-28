40b028ccc041476aaca277fd6158bc83bec17b67
Manan Jain <manan@wirelessprv-10-194-163-75.near.illinois.edu>
2022-03-28 10:37:49 -0500

    Added button style and animations, Single image capture is now functional

--- frontend/src/components/UploadImage.js
+++ frontend/src/components/UploadImage.js
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 import Webcam from "react-webcam";
+
 
 const WebcamComponent = () => <Webcam />;
 
-const UploadImage = () => {
-  return (
-    <div>
-      <div className="Webcam">
-        <Webcam
-          audio={false}
-          width={"50%"}
-          screenshotFormat="image/png"  
-        >
-
-        {({ getScreenshot }) => (
-          <button
-            onClick={() => {
-              const imageSrc = getScreenshot();
-            }}
-          >
-            Capture photo
-          </button>
-        )}
-        </Webcam>
-
-        
-      </div>
-    </div>
-  );
+const videoConstraints = {
+    width: 220,
+    height: 200,
+    facingMode: "user"
 };
 
-export default UploadImage;
+export const WebcamCapture = () => {
+
+    const [image,setImage]=useState('');
+    const webcamRef = React.useRef(null);
+
+    
+    const capture = React.useCallback(
+        () => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImage(imageSrc)
+        });
+
+
+    return (
+        <div className="webcam-container">
+            <div className="webcam-img">
+
+                {image == '' ? <Webcam style={{height: "100%", borderRadius: "20px"}}
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={videoConstraints}
+                /> : <img style={{height: "100%", borderRadius: "20px"}} src={image} />}
+            </div>
+            <div>
+                {image != '' ?
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        setImage('')
+                    }}
+                        className="webcam-btn">
+                        Retake Image</button> :
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        capture();
+                    }}
+                        className="webcam-btn">Capture</button>
+                }
+            </div>
+        </div>
+    );
+};
+
+
+export default WebcamCapture;
